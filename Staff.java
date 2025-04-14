4d375dd570cc1172edbd87b98eda5981d6c135ee
LazoMcCarroll <LazoMcCarroll@protonmail.com>
2025-04-13 20:27:57 -0600

    Delete Staff.java

--- Staff.java
+++
@@ -1,8 +0,0 @@
-/**
- * Class representing a staff member, extends the Person class.
- */
-public class Staff extends Person {
-    public Staff(String name, String email, String password) {
-        super(name, email, password);
-    }
-}
