6822b542414fb9e0919239d64d63a86768f29d4b
LazoMcCarroll <LazoMcCarroll@protonmail.com>
2025-04-13 20:27:05 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main 
-{
-    public static void main(String[] args)
-    {
-        // Initialize the MovieTheaterSystem
-        MovieTheaterSystem system = new MovieTheaterSystem();
-    }
-}
