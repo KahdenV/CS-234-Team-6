35bc929bebebb3460046f4a94ec5b2feeaf99d94
KahdenV <kahdenv@gmail.com>
2025-05-08 09:25:54 -0600

    Updated Login Window GUI
    Improved the look of the Login Window

--- Part4/src/netbeans_gui/LoginWindow_GUI.java
+++ Part4/src/netbeans_gui/LoginWindow_GUI.java
@@ -56,31 +56,34 @@ public class LoginWindow_GUI extends javax.swing.JFrame {
         Login_Button = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login Window");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setMinimumSize(new java.awt.Dimension(640, 360));
+        setMinimumSize(new java.awt.Dimension(660, 410));
         setPreferredSize(new java.awt.Dimension(640, 360));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        Login_Label.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
+        Login_Label.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         Login_Label.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Login_Label.setText("Login");
-        getContentPane().add(Login_Label, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 40, -1, -1));
+        getContentPane().add(Login_Label, new org.netbeans.lib.awtextra.AbsoluteConstraints(265, 110, -1, -1));
 
         UserInput_TextField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        UserInput_TextField.setToolTipText("Username");
+        UserInput_TextField.setToolTipText("Email");
         UserInput_TextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 UserInput_TextFieldActionPerformed(evt);
             }
         });
-        getContentPane().add(UserInput_TextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 120, 180, 30));
+        getContentPane().add(UserInput_TextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 170, 180, 30));
 
         PasswordInput_PasswordField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         PasswordInput_PasswordField.setToolTipText("Password");
-        getContentPane().add(PasswordInput_PasswordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 160, 180, 30));
+        getContentPane().add(PasswordInput_PasswordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 210, 180, 30));
 
         Login_Button.setText("Login");
         Login_Button.addActionListener(new java.awt.event.ActionListener() {
@@ -88,7 +91,7 @@ public class LoginWindow_GUI extends javax.swing.JFrame {
                 Login_ButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(Login_Button, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 200, 140, 30));
+        getContentPane().add(Login_Button, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 270, 140, 30));
 
         jButton1.setText("Create Account");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -96,7 +99,7 @@ public class LoginWindow_GUI extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 240, 140, 30));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 310, 140, 30));
 
         jButton2.setText("Skip");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -104,7 +107,18 @@ public class LoginWindow_GUI extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 280, 100, -1));
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 340, 100, -1));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Movie Munch Theater");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 10, 540, 80));
+
+        jLabel2.setText("Email:");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 180, -1, -1));
+
+        jLabel3.setText("Password:");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 220, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
@@ -224,5 +238,8 @@ public class LoginWindow_GUI extends javax.swing.JFrame {
     private javax.swing.JTextField UserInput_TextField;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
